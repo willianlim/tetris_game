e304ffc4ecd60c7336dbaa436e1cbf8f9329d229
Willian-code <willianrosendodelima@outlook.com>
2022-09-24 21:14:58 -0300

    working with arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-	
-})
+function main() {
+	let	grid;
+	let	width;
+	let	squares;
+	let	startBtn;
+	let	scoreDisplay;
+
+	width = 10;
+	grid = document.querySelector('.grid');
+	squares = Array.from(document.querySelectorAll('.grid div'));
+}
+
+document.addEventListener("DOMContentLoaded", main)
