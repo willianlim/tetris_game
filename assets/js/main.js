9c2900e368216056b45d5ce02a0e3db68ff7c211
Willian-code <willianrosendodelima@outlook.com>
2022-09-26 15:14:54 -0300

    setting the canvas and creating an array for teh coordinates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,49 @@
-import { shapesTetrominos } from "./shapesTetrominos.js"
+let	canvas;
+let	ctx;
+let	gBArrayHeight = 20;
+let	gBArrayWidth = 12;
+let	startX = 4;
+let	startY = 0;
+let	coordinateArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth).fill(0))
+let	curTetromino = [[1, 0], [0, 1], [1, 1], [2, 1]];
 
-function main() {
-	const	WIDTH = 10;;
-	const	GRID = document.querySelector('.grid');;
-	const	SQUARES = Array.from(document.querySelectorAll('.grid div'));;
-	let		startBtn;
-	let		scoreDisplay;
-	const	theTetrominoes = shapesTetrominos(WIDTH);
+class Coordinates {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
 }
 
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener('DOMContentLoaded', SetupCanvas);
+
+function CreateCoordArray() {
+	let	i;
+	let	j;
+
+	j = 0;
+	for (let y = 9; y <= 446; y += 23) {
+		i = 0;
+		for (let x = 11; x <= 264; x += 23) {
+			coordinateArray[i][j] = new Coordinates(x, y);
+			i++;
+		}
+		j++;
+	}
+}
+
+function SetupCanvas() {
+	canvas = document.getElementById('my-canvas');
+	ctx = canvas.getContext('2d');
+	canvas.width = 936;
+	canvas.height = 956;
+
+	ctx.scale(2, 2);
+
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = 'black';
+	ctx.strokeRect(8, 8, 280, 462);
+
+	CreateCoordArray();
+}
